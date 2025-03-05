28e39955ee5759e25193f9348e47e24ce9e49bae
Zhaoyi (Astor) Song <36931541+AstorSong@users.noreply.github.com>
2025-03-05 15:12:01 -0500

    Add files via upload

---
+++ swe-digital-library/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swe-digital-library
+//
+//  Created by Astor Song on 2025-03-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swe-digital-library/swe_digital_libraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  swe_digital_libraryApp.swift
+//  swe-digital-library
+//
+//  Created by Astor Song on 2025-03-05.
+//
+
+import SwiftUI
+
+@main
+struct swe_digital_libraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
